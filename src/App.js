c72f0e2b10be633f0f105079f130c8c849718105
haikalkilkul <ashidqi.haikal@gmail.com>
2022-10-23 10:28:16 +0700

    hide list and styling scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import dataAegis from './dataAegis';
-import { useState, useRef } from 'react';
+import "./Custom.scss";
+import dataAegis from "./dataAegis";
+import { useState, useRef } from "react";
 
 function App() {
   const [data, setData] = useState(dataAegis.slice(0, 10));
   const inputRef = useRef(null);
+  const [showInfo, setShowInfo] = useState(false);
 
   const handleClick = () => {
     const inputValue = inputRef.current.value;
+    setShowInfo((current) => (current = true));
+    console.log(showInfo);
     if (!inputValue) return setData(dataAegis.slice(0, 10));
     setData(
       dataAegis
@@ -28,59 +31,90 @@ function App() {
 
   return (
     <>
-      <input type='text' ref={inputRef} />
-      <button onClick={handleClick}>Submit</button>
-      {data.map((value, index) => (
-        <div key={index}>
-          <h2>{value.nama_lengkap}</h2>
-          <br />
-          <table>
-            <tbody>
-              <tr>
-                <td>Nama Lengkap</td>
-                <td>:</td>
-                <td>{value.nama_lengkap || '-'}</td>
-              </tr>
-              <tr>
-                <td>Nama Panggilan</td>
-                <td>:</td>
-                <td>{value.nama_panggilan || '-'}</td>
-              </tr>
-              <tr>
-                <td>NIM</td>
-                <td>:</td>
-                <td>{value.nim || '-'}</td>
-              </tr>
-              <tr>
-                <td>Nomor Telepon</td>
-                <td>:</td>
-                <td>{value.nomor_telepon || '-'}</td>
-              </tr>
-              <tr>
-                <td>ID Line</td>
-                <td>:</td>
-                <td>{value.id_line || '-'}</td>
-              </tr>
-              <tr>
-                <td>Email</td>
-                <td>:</td>
-                <td>{value.email || '-'}</td>
-              </tr>
-              <tr>
-                <td>Hobi</td>
-                <td>:</td>
-                <td>{value.hobi || '-'}</td>
-              </tr>
-              <tr>
-                <td>Tanggal Lahir</td>
-                <td>:</td>
-                <td>{value.tanggal_lahir || '-'}</td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
+    <h1>Praktikum PRLBK Kelompok 1</h1>
+      <div class="Card">
+        <div class="CardInner">
+          <label>Tugas Modul 2 Kelompok 1</label>
+          <div class="container">
+          <button onClick={handleClick}>
+            <div class="Icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#657789"
+                stroke-width="3"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-search"
+              >
+                <circle cx="11" cy="11" r="8" />
+                <line x1="21" y1="21" x2="16.65" y2="16.65" />
+              </svg>
+            </div>
+            </button>
+            <div class="InputContainer">
+              <input placeholder="Find Aegis" type="text" ref={inputRef} />
+
+            </div>
+          </div>
         </div>
-      ))}
+      </div>
+      <div style={{ display: showInfo ? "block" : "none" }}>
+        {data.map((value, index) => (
+          <div key={index}>
+            <h2 style={{ textAlign: "center" }}>{value.nama_lengkap}</h2>
+            <br />
+            <table>
+              <tbody>
+                <tr>
+                  <td>Nama Lengkap</td>
+                  <td>:</td>
+                  <td>{value.nama_lengkap || "-"}</td>
+                </tr>
+                <tr>
+                  <td>Nama Panggilan</td>
+                  <td>:</td>
+                  <td>{value.nama_panggilan || "-"}</td>
+                </tr>
+                <tr>
+                  <td>NIM</td>
+                  <td>:</td>
+                  <td>{value.nim || "-"}</td>
+                </tr>
+                <tr>
+                  <td>Nomor Telepon</td>
+                  <td>:</td>
+                  <td>{value.nomor_telepon || "-"}</td>
+                </tr>
+                <tr>
+                  <td>ID Line</td>
+                  <td>:</td>
+                  <td>{value.id_line || "-"}</td>
+                </tr>
+                <tr>
+                  <td>Email</td>
+                  <td>:</td>
+                  <td>{value.email || "-"}</td>
+                </tr>
+                <tr>
+                  <td>Hobi</td>
+                  <td>:</td>
+                  <td>{value.hobi || "-"}</td>
+                </tr>
+                <tr>
+                  <td>Tanggal Lahir</td>
+                  <td>:</td>
+                  <td>{value.tanggal_lahir || "-"}</td>
+                </tr>
+              </tbody>
+            </table>
+            <br />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
